7681f3415f5c2871e42c46bc41c895985a5a22db
aditya1q <129264670+aditya1q@users.noreply.github.com>
2023-09-10 13:48:33 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,10 +2,16 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  output: 'export',
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' }, // Define the export path for the root page
+      // Add more export paths for other pages if needed
+    };
+  },
+  export: 'output', // Specify the export directory
   images: {
     domains: ["cdn.imagin.studio"],
   },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
